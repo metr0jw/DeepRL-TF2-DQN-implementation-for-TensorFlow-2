70d62b023142f0653cec79c53ca5369876e732e6
Jiwoon Lee <metr0jw@naver.com>
2021-12-31 02:10:24 +0900

    chore: Fix train.py
    - Optimized imports
    - Changed device from CPU to GPU

--- train.py
+++ train.py
@@ -1,10 +1,11 @@
 from env import MaxAndSkipEnv, FireResetEnv, ProcessFrame84, ImageToTensor, BufferWrapper, ScaledFloatFrame
+from experience import ExperienceReplay, Experience
 from agent import Agent
 import models
-from experience import ExperienceReplay, Experience
+
+from tensorflow.keras.optimizers import Adam
 import numpy as np
 import gym
-import gym.spaces
 
 
 def make_env(env_name):
@@ -17,6 +18,7 @@ def make_env(env_name):
     return ScaledFloatFrame(environ)
 
 
+DEVICE = "GPU"
 DEFAULT_ENV_NAME = "PongNoFrameskip-v4"     # Name of environment to train
 MEAN_REWARD_BOUND = 19.0                    # The boundary of reward to stop training
 gamma = 0.99                                # Discount factor
@@ -35,13 +37,13 @@ print(env.action_space.n)
 print(env.observation_space.shape)
 
 net = models.DQN(env.observation_space.shape,
-                 env.action_space.n).to(device)
+                 env.action_space.n).to(DEVICE)
 target_net = models.DQN(env.observation_space.shape,
-                        env.action_space.n).to(device)
+                        env.action_space.n).to(DEVICE)
 buffer = ExperienceReplay(replay_size)
 agent = Agent(env, buffer)
 epsilon = eps_start
-optimizer = optim.Adam(net.parameters(), lr=learning_rate)
+optimizer = Adam(net.parameters(), lr=learning_rate)
 total_rewards = []
 frame_idx = 0
 best_mean_reward = None
@@ -49,7 +51,7 @@ best_mean_reward = None
 while True:
     frame_idx += 1
     epsilon = max(epsilon*eps_decay, eps_min)
-    reward = agent.play_step(net, epsilon, device=device)
+    reward = agent.play_step(net, epsilon, device=DEVICE)
     if reward is not None:
         total_rewards.append(reward)
         mean_reward = np.mean(total_rewards[-100:])
